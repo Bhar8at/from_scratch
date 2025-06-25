1f63a33db2a53cf38930f530a6ae0b5c54c17c97
Bharat Deleep Nair <bhtnair@gmail.com>
2025-06-25 12:21:04 +0530

    implemented a convolution layer, reshape, layer and binary cross entropy loss

--- NN/activations.py
+++ NN/activations.py
@@ -8,3 +8,16 @@ class Tanh(activation):
         def tanh_prime(x):
             return 1 - np.tanh(x) ** 2
         super().__init__(tanh, tanh_prime)
+
+
+class  Sigmoid(activation):
+    def __init__(self):
+        def sigmoid(x):
+            return 1/(1+np.exp(-x))
+        def sigmoid_prime(x):
+            s = sigmoid(x)
+            return s * (1 - s)
+        super().__init__(sigmoid, sigmoid_prime)
+
+
+        
